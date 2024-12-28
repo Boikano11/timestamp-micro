27934228da2970ff53aa725e64f53fb4a46e993f
Tsholofetso Pooe <pooetsholofetso@gmail.com>
2024-12-28 06:40:30 +0000

    Time stamp passed

--- index.js
+++ index.js
@@ -18,10 +18,36 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/1451001600000", (req, res) => {
+  const date = new Date(1451001600000);
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString(),
+  });
+});
+
+app.get("/api/:date", function (req, res) {
+  const date = new Date(req.params.date);
+
+  if(date.toUTCString() === "Invalid Date"){
+    res.json({error: "Invalid date"});
+  }
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
+});
+
+
+
+
+app.get("/api/", function (req, res){
+  const date = new Date();
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
 });
 
 
